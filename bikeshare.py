8a31cf0dc87f50793fe87327d35f1e3d811bde89
Masaru Wakatsuru <m.wakatsuru@gmail.com>
2019-02-24 16:13:04 +0900

    Add the explanation for the restart question

--- bikeshare.py
+++ bikeshare.py
@@ -217,6 +217,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+        # If you want to restart, please press 'yes'
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
